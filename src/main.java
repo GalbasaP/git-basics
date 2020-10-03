c15cb822d2792e9bede4a10b9b19a1d52aad8c13
Artiom Loiko <artiom.loiko@gmail.com>
2020-10-03 23:23:12 +0300

    changed main.java

--- src/main.java
+++ src/main.java
@@ -1,7 +1,7 @@
 public class main {
 
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
+        int a = 11;
+        System.out.println(a+" Hello world!");
     }
 }
